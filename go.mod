4b5b3b108c08015bc95b054aee94ed778e8fa22e
m01e <tmacfalcon@gmail.com>
2024-01-22 23:55:30 +0800

    Finish: JDWP Handshake

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fa1c0n1/jdwp-codeifier-go
+
+go 1.20
---
+++ jdwp-codeifier.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+)
+
+type JDWPClient struct {
+}
+
+const (
+	JDWP_HANDSHAKE = "JDWP-Handshake"
+)
+
+func (jdwp *JDWPClient) connect(host string, port string) (bool, error) {
+	targetAddr := net.JoinHostPort(host, port)
+	fmt.Println("targetAddr:", targetAddr)
+
+	conn, err := net.Dial("tcp", targetAddr)
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
+
+	defer conn.Close()
+
+	n, err := conn.Write([]byte(JDWP_HANDSHAKE))
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
+
+	fmt.Println("send, n=", n)
+	readBs := make([]byte, len(JDWP_HANDSHAKE))
+	n, err = conn.Read(readBs)
+	if n != len(JDWP_HANDSHAKE) {
+		return false, errors.New("Fail to jdwp handshake!!!")
+	}
+
+	fmt.Println("recv:", string(readBs[:n]))
+	fmt.Println("JDWP Handshake successfully.")
+	return true, nil
+}
+
+func (jdwp *JDWPClient) CreatePacket() {
+	fmt.Println("Create Packet")
+}
+
+func main() {
+	jdwpClient := new(JDWPClient)
+	bRet, err := jdwpClient.connect("104.199.140.152", "8777")
+	if !bRet {
+		panic(err)
+	}
+}
